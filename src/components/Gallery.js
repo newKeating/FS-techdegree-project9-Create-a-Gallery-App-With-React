b50d0d0c684dcaae319dbe164767be0f8cb859f8
Peter Kim <kroits09@gmail.com>
2018-07-01 00:48:13 +0900

    Display photos loop

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,22 +1,19 @@
 import React from 'react';
 
 const Gallery = (props) => {
+  const { photos } = props;
+  const galleryImages = photos.map((photo) => {
+    return (
+      <li key={photo.id}>
+        <img src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} alt={photo.title}/>
+      </li>
+    );
+  }); 
   return (
     <div className="photo-container">
       <h2>Results</h2>
       <ul>
-        <li>
-          <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
-        </li>
-        <li>
-          <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
-        </li>
-        <li>
-          <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
-        </li>
-        <li>
-          <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-        </li>
+        {galleryImages}
       </ul>
     </div>
   );
